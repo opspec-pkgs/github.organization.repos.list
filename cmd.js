d82811414da2c93759f37e287c32942483310e96
Chris Dostert <chris.dostert@live.com>
2018-02-25 18:46:16 -0800

    chore) initial commit

---
+++ cmd.js
@@ -0,0 +1,30 @@
+const octokit = require('@octokit/rest')();
+const fs = require('fs');
+
+// token (https://github.com/settings/tokens)
+octokit.authenticate({
+  type: 'token',
+  token: process.env.accessToken,
+})
+
+/**
+ * Paginates a call
+ * @param {Function} call 
+ */
+async function paginate (call) {
+  let response = await call({per_page: 100})
+  let {data} = response
+  while (octokit.hasNextPage(response)) {
+      response = await octokit.getNextPage(response)
+      data = data.concat(response.data)
+  }
+  return data
+}
+
+paginate(() => octokit.repos.getAll({org: process.env.org}))
+  .then(data => {
+    fs.writeFileSync('/repos', JSON.stringify(data));
+  }).catch(err => {
+      console.log(err.message);
+      process.exit(1);
+  });
